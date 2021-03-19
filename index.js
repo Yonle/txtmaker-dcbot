4ad937f540eedcea7836a1e9b9e1e12b75a16841
Yonle <63401712+Yonle@users.noreply.github.com>
2021-03-19 22:46:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const bot = new Client();
 bot.on('message', message => {
 	var text = message.content;
 	message.channel.send({
-                files: [
+                files: [{
 		  attachments: (() => new Readable.from(text.toString()))(),
 		  name: `${message.author.name}.txt`
-                ]
+                }]
 	}).catch(console.error);
 });
 bot.login("Y o u r  b o t  t o k e n").then(() => console.log("Ready")).catch(console.error);
